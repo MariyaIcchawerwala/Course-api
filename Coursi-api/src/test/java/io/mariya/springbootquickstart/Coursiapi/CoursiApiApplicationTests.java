8551195ff4360b35092a88255441ca8be7a26867
Mariya Icchawerwala <mariyaicchawer@gmail.com>
2022-10-06 11:49:02 +0530

    Add files via upload

---
+++ Coursi-api/src/test/java/io/mariya/springbootquickstart/Coursiapi/CoursiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mariya.springbootquickstart.Coursiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
